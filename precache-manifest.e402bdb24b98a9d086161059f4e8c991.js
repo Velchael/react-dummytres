14209f2dc23ebf70e895b413b6aa7701c8292cb3
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-01-06 01:00:40 -0300

    Updates

---
+++ precache-manifest.e402bdb24b98a9d086161059f4e8c991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4506b77a140fa92cba66",
+    "url": "/react-dummytres/static/css/main.9bd0c474.chunk.css"
+  },
+  {
+    "revision": "4506b77a140fa92cba66",
+    "url": "/react-dummytres/static/js/main.4506b77a.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/react-dummytres/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "8a7b5155ec74715eaeb5",
+    "url": "/react-dummytres/static/js/2.8a7b5155.chunk.js"
+  },
+  {
+    "revision": "ed7954cf0bc254b62afb",
+    "url": "/react-dummytres/static/js/runtime~main.ed7954cf.js"
+  },
+  {
+    "revision": "954749d7429d40463ded6de8ca65c8ac",
+    "url": "/react-dummytres/static/media/pexels-lukas-574071tres.954749d7.jpg"
+  },
+  {
+    "revision": "ee6d75f338e434912e1a632442db375f",
+    "url": "/react-dummytres/static/media/logo.ee6d75f3.svg"
+  },
+  {
+    "revision": "fcbd23e09c3a20f17174208edca1a254",
+    "url": "/react-dummytres/index.html"
+  }
+];
